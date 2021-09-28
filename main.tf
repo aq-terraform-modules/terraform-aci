a478f1b85645ec31997d18ecb611f01deb8731d6
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-09-29 01:02:18 +0700

    Add ACI resource and related variables

---
+++ main.tf
@@ -0,0 +1,47 @@
+resource "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+  location = var.location
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+
+  tags = {
+    name            = var.resource_group_name
+  }
+}
+
+resource "azurerm_container_group" "aci" {
+  name = var.aci_name
+  location = var.location
+  resource_group_name = var.resource_group_name
+  ip_address_type = var.ip_address_type
+  dns_name_label = var.dns_name_label
+  os_type = var.os_type
+  restart_policy = var.restart_policy
+
+  container {
+    name = var.container_name
+    image = var.container_image
+    cpu = var.cpu
+    memory = var.memory
+    environment_variables = var.environment_variables
+    secure_environment_variables = var.secure_environment_variables
+
+    dynamic "ports" {
+      for_each = var.container_ports
+      content {
+        port = ports.value.port
+        protocol = ports.value.protocol
+      }
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+
+  tags = {
+    name            = var.aci_name
+  }
+}
--- variable.tf
+++ variable.tf
@@ -1,4 +1,77 @@
 variable "resource_group_name" {
   description = "Name of the RG that will contain related resources"
+  type = string
 }
 
+variable "location" {
+  description = "Location of the resources"
+  type = string
+}
+
+variable "aci_name" {
+  description = "Name of the Azure ACI"
+  type = string
+}
+
+variable "ip_type" {
+  description = "IP address type. Should be public or private"
+  type = string
+  default = "public"
+}
+
+variable "dns_name_label" {
+  description = "DNS name for the ACI"
+  type = string
+}
+
+variable "os_type" {
+  description = "OS type of the ACI. Should be Linux or Windows"
+  type = string
+  default = "Linux"
+}
+
+variable "container_name" {
+  description = "Name of the container inside the ACI"
+  type = string
+}
+
+variable "container_image" {
+  description = "Image of the container"
+  type = string
+}
+
+variable "cpu" {
+  description = "Total CPU for the container"
+  type = string
+  default = 1
+}
+
+variable "memory" {
+  description = "Total Memory for the container"
+  type = string
+  default = 1
+}
+
+variable "container_ports" {
+  description = "Exposed for for the container"
+  type = any
+  default = []
+}
+
+variable "restart_policy" {
+  description = "Restart policy for the ACI. Should be Always, Never, OnFailure"
+  type = string
+  default = "Always"
+}
+
+variable "environment_variables" {
+  description = "Environment vars for the container"
+  type = map
+  default = {}
+}
+
+variable "secure_environment_variables" {
+  description = "Environment vars for the container"
+  type = map
+  default = {}
+}
